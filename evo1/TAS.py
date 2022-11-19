5d7de11d3f441024cb40f194d323cb275e4816f6
orkaboy <orkaboy@gmail.com>
2022-11-19 18:51:40 +0100

    Added to the forest pathing

--- evo1/TAS.py
+++ evo1/TAS.py
@@ -106,5 +106,60 @@ class FirstArea(SeqList):
                 SeqAttack("Bush"),
                 SeqMove2D("Move to bush", coords=[Vec2(32, 55)]),
                 SeqGrabChest("Monsters", direction=Facing.RIGHT),
+                # TODO: Should add annotator here that can deal with enemies (seems ok tho)
+                SeqMove2D(
+                    "Dodge enemies",
+                    coords=[
+                        Vec2(35, 55),
+                        Vec2(35, 54),
+                        Vec2(36, 54),
+                        Vec2(36, 53),
+                        Vec2(37, 53),
+                        Vec2(37, 52),
+                        Vec2(39, 52),
+                    ],
+                ),
+                # Here's music chest to the right, TODO: optionally grab?
+                SeqAttack("Bush"),
+                SeqMove2D(
+                    "Move to chest",
+                    coords=[
+                        Vec2(39, 47),
+                        # Optional, chest to the north, save (move to Vec2(39, 45), then open chest N)
+                        Vec2(41, 47),
+                        Vec2(41, 48),
+                        Vec2(44, 48),
+                        Vec2(44, 49),
+                    ],
+                ),
+                SeqGrabChest("16-bit", direction=Facing.DOWN),
+                # TODO: Some enemies here, will probably fail
+                SeqMove2D(
+                    "Dodge enemies",
+                    coords=[
+                        Vec2(44, 52),
+                        Vec2(50, 52),
+                        Vec2(50, 53),
+                        Vec2(55, 53),
+                        Vec2(55, 54),
+                        Vec2(58, 54),
+                    ],
+                ),
+                # TODO: WR attacks bush here, needed or just swag?
+                SeqMove2D(
+                    "Dodge enemies",
+                    coords=[
+                        Vec2(60, 54),
+                        Vec2(60, 44),
+                        Vec2(51, 44),
+                        Vec2(51, 45),
+                        Vec2(48, 45),
+                        Vec2(48, 36),
+                        Vec2(36, 36),
+                        Vec2(36, 33),
+                        Vec2(34, 33),
+                    ],
+                ),
+                SeqGrabChest("Free move", direction=Facing.LEFT),
             ],
         )
