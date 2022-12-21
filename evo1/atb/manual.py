36d5fefa4ea7b32e429448f0449262a324b0c343
orkaboy <orkaboy@gmail.com>
2022-12-21 19:21:49 +0100

    Fixed a bug in manual atb combat

--- evo1/atb/manual.py
+++ evo1/atb/manual.py
@@ -3,8 +3,8 @@ from evo1.atb.base import SeqATBCombat
 
 # Dummy class for ATB combat testing; requires manual control
 class SeqATBCombatManual(SeqATBCombat):
-    def __init__(self, name: str = "Generic", wait_for_battle: bool = False) -> None:
-        super().__init__(name=name, wait_for_battle=wait_for_battle)
+    def __init__(self, name: str = "Generic") -> None:
+        super().__init__(name=name)
 
     def handle_combat(self, should_run: bool = False):
         pass
