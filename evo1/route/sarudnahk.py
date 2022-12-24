9410aa8d9cd750900fe9b7c70f6f949998d807b5
orkaboy <orkaboy@gmail.com>
2022-12-24 10:25:34 +0100

    Initial stub include of sarudnahk

--- evo1/route/sarudnahk.py
+++ evo1/route/sarudnahk.py
@@ -1,4 +1,22 @@
+from engine.combat import SeqMove2DClunkyCombat
+from engine.mathlib import Vec2
 from engine.seq import SeqList
+from memory.evo1 import get_diablo_memory
+
+
+# TODO: Should maybe inherit SeqMove2D instead, since we usually don't want to attack everything that moves here (slow)
+class SeqDiabloCombat(SeqMove2DClunkyCombat):
+    def __init__(self, name: str, coords: list[Vec2], precision: float = 0.2):
+        # TODO: Could use func here to load_diablo_memory?
+        super().__init__(name, coords, precision)
+        self.mem = get_diablo_memory()
+
+    # TODO: execute, render
+
+    # TODO: Boid behavior?
+
+
+# TODO: NavMap for Sarudnahk
 
 
 class Sarudnahk(SeqList):
@@ -6,6 +24,7 @@ class Sarudnahk(SeqList):
         super().__init__(
             name="Sarudnahk",
             children=[
+                SeqDiabloCombat("Move to chest", coords=[]),  # TODO
                 # TODO: switch to Kaeris
                 # TODO: Navigate through the Diablo section (Boid behavior?)
                 # TODO: Fight the Undead King boss (abuse gravestone hitbox)
